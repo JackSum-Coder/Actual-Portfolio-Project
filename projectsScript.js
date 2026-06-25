81aa574f3a44718d156b333f7e44c1f52b3a604f
Jack Sommerville <sommerjack7099@gmail.com>
2026-06-25 15:04:09 -0500

    linked projects

---
+++ projectsScript.js
@@ -0,0 +1,7 @@
+let project3 = document.getElementById("project3")
+
+if (project3) {
+    project3.addEventListener("click", function() {
+        window.open("https://jacksum-coder.github.io/Choose-Adventure-Game/", "_blank", "noopener,noreferrer");
+    })
+}
