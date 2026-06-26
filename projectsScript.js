b83b736543c6fe2ad3f165b3137f2bd4c631f47a
Jack Sommerville <sommerjack7099@gmail.com>
2026-06-26 09:27:39 -0500

    save

--- projectsScript.js
+++ projectsScript.js
@@ -1,7 +1,12 @@
 let project3 = document.getElementById("project3")
 
-if (project3) {
-    project3.addEventListener("click", function() {
-        window.open("https://jacksum-coder.github.io/Choose-Adventure-Game/", "_blank", "noopener,noreferrer");
-    })
-}
+
+project3.addEventListener("click", function() {
+    window.open("https://jacksum-coder.github.io/Choose-Adventure-Game/", "_blank", "noopener,noreferrer");
+})
+let project2 = document.getElementById("project2")
+
+
+project2.addEventListener("click", function() {
+    window.open("https://jacksum-coder.github.io/freedom_of_speech/", "_blank", "noopener,noreferrer");
+})
