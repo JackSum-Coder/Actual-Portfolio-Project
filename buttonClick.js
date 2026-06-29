096d30b080086423c6d60ea92f403aa4208879c7
Jack Sommerville <sommerjack7099@gmail.com>
2026-06-29 14:22:18 -0500

    finsied

---
+++ buttonClick.js
@@ -0,0 +1,13 @@
+let youtube = document.querySelector("#youtube")
+youtube.addEventListener("click", function() {
+    window.open("https://www.youtube.com/@JackStacks_1", "_blank", "noopener,noreferrer");
+})
+let linkedin = document.querySelector("#linkedIn")
+linkedin.addEventListener("click", function() {
+    window.open("https://www.linkedin.com/in/jack-sommerville-121a31395", "_blank", "noopener,noreferrer");
+})
+let github = document.querySelector("#github")
+github.addEventListener("click", function(){
+        
+        window.open("    https://github.com/JackSum-Coder", "_blank", "noopener,noreferrer");
+})
